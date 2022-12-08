e0ae9cea74b498f99e0c3cddd081cf585fcd8fec
Ahmed Khalaf <pxlshpr@gmail.com>
2022-12-09 00:13:48 +0500

    bump

--- Sources/App/Models/UserFastingTimer.swift
+++ Sources/App/Models/UserFastingTimer.swift
@@ -39,3 +39,24 @@ extension UserFastingTimer {
         self.nextMealName = form.nextMealName
     }
 }
+
+/**
+ 
+ SQL:
+ 
+ ```
+ SELECT
+     x.id,
+     x.last_notification_hour,
+     x.hours
+ FROM
+ (
+     SELECT
+         *,
+         FLOOR(((CAST(EXTRACT(epoch FROM NOW()) AS INT) - last_meal_at) / 3600)) as hours
+     FROM user_fasting_timers
+ ) AS x
+ where x.hours > x.last_notification_hour;
+ ```
+ 
+ */
