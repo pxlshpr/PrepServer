bc9faaddf26e8e16f6be631d3ec5f81700c6d7df
Ahmed Khalaf <pxlshpr@gmail.com>
2022-12-09 14:46:33 +0500

    bump

--- Sources/App/Jobs/FastingActivityUpdateJob.swift
+++ Sources/App/Jobs/FastingActivityUpdateJob.swift
@@ -11,37 +11,47 @@ struct FastingActivityUpdateJob: AsyncScheduledJob {
     
     func run(context: QueueContext) async throws {
         
-//        let updates = try await FastingActivityController().getActivitiesPendingUpdate(on: context.application.db)
-//        for update in updates {
-//            print("Send notification to: \(update.id)")
-//        }
-        
+        let activities = try await FastingActivityController().getActivitiesPendingUpdate(on: context.application.db)
+        for activity in activities {
+            print("💼 Updating activity")
+            try await update(activity, app: context.application)
+        }
+    }
+    
+    func update(_ activity: UserFastingActivity, app: Application) async throws {
         do {
-            print("getting first activity")
-            guard let firstActivity = try await UserFastingActivity.query(on: context.application.db)
-                .filter(\.$deletedAt == nil)
-                .first() else {
-                return
-            }
-            print("posting test notification to: \(firstActivity.pushToken)")
-            try await sendNotification(for: firstActivity, application: context.application)
+            print("💼 Posting notification to: \(activity.pushToken)")
+            try await sendNotification(for: activity, app: app)
             return
         } catch {
             print("Error running job: \(error)")
             return
         }
-
     }
 }
 
-func sendNotification(for activity: UserFastingActivity, application: Application) async throws {
+extension UserFastingActivity {
+    var fastingState: FastingTimerState {
+        FastingTimerState(
+            lastMealTime: <#T##Date#>,
+            nextMeal: <#T##DayMeal?#>,
+            countdownType: <#T##FastingTimerCountdownType#>
+        )
+    }
+
+    var contentState: FastingActivityContentState {
+        FastingActivityContentState(fastingState: fastingState)
+    }
+}
+
+func sendNotification(for activity: UserFastingActivity, app: Application) async throws {
     //TODO: Send actual lastMealTime
     let fastingState = FastingTimerState(
         lastMealTime: Date().moveDayBy(-2)
     )
     let contentState = FastingActivityContentState(fastingState: fastingState)
     
-    try await application.apns.client.sendLiveActivityNotification(
+    try await app.apns.client.sendLiveActivityNotification(
         .init(
             expiration: .immediately,
             priority: .immediately,
