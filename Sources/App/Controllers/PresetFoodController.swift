48b10c3d6c228391a8e5be4b57d0f8e1277c4d4f
Ahmed Khalaf <pxlshpr@gmail.com>
2022-12-16 11:36:42 +0500

    bump

--- Sources/App/Controllers/PresetFoodController.swift
+++ Sources/App/Controllers/PresetFoodController.swift
@@ -10,24 +10,47 @@ struct PresetFoodController: RouteCollection {
     
     func create(req: Request) async throws -> HTTPStatus {
         let form = try req.content.decode(PresetFoodForm.self)
-        print("Processing form: \(form.food.emoji) \(form.food.name)")
         
-        let presetFood = PresetFood(food: form.food, dataset: form.dataset, datasetFoodId: form.id)
-        print("Created PresetFood, saving")
-        
-        try await presetFood.save(on: req.db)
-
-        /// Save any barcodes
-        print("Saving Barcodes")
-        for foodBarcode in form.food.info.barcodes {
-            let barcode = Barcode(
-                foodBarcode: foodBarcode,
-                presetFoodId: try presetFood.requireID()
-            )
-            try await barcode.save(on: req.db)
+        if let existingFood = try await PresetFood.query(on: req.db)
+            .filter(\.$datasetFoodId == form.id)
+            .filter(\.$dataset == form.dataset)
+            .first()
+        {
+            existingFood.updateMetadata(with: form.food)
+            try await existingFood.update(on: req.db)
+            print("Updated: \(form.food.emoji) \(form.food.name)")
+        } else {
+            let presetFood = PresetFood(food: form.food, dataset: form.dataset, datasetFoodId: form.id)
+            try await presetFood.save(on: req.db)
+            print("Created: \(form.food.emoji) \(form.food.name)")
         }
+
+        //TODO: Bring back barcodes when we handle existing barcodes first
+//        /// Save any barcodes
+//        for foodBarcode in form.food.info.barcodes {
+//            let barcode = Barcode(
+//                foodBarcode: foodBarcode,
+//                presetFoodId: try presetFood.requireID()
+//            )
+//            try await barcode.save(on: req.db)
+//        }
         
-        print("Created: \(form.food.emoji) \(form.food.name)")
         return .ok
+    }
+}
+
+extension PresetFood {
+    func updateMetadata(with food: Food) {
+        self.name = food.name
+        self.emoji = food.emoji
+        self.amount = food.info.amount
+        self.nutrients = food.info.nutrients
+        self.sizes = food.info.sizes
+        self.serving = food.info.serving
+        self.detail = food.detail
+        self.brand = food.brand
+        self.density = food.info.density
+        
+        self.updatedAt = Date().timeIntervalSince1970
     }
 }
--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@ unixtime=$(date +%s)
 cd /home/ahmed/PrepBackups
 #file="prep-backup-${unixtime}.sql"
 file="prep-backup.sql"
-pg_dump -U pxlshpr prep > $file
+pg_dump -U pxlshpr prep --exclude-table-data "preset_foods" > $file
 echo "         💾 Backup saved to: ${file}"
 git add .
 git commit -a -m "added backup"
