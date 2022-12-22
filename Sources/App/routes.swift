6bc027318949c8bd6a620879f9ddabd449a28afe
Ahmed Khalaf <pxlshpr@gmail.com>
2022-12-22 19:18:03 +0500

    bump

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,7 +6,8 @@ func routes(_ app: Application) throws {
     try app.register(collection: SyncController())
     try app.register(collection: FastingActivityController())
     try app.register(collection: PresetFoodController())
-    
+    try app.register(collection: CleanupController())
+
     app.get("backup") { req in
         try shell("./backup.sh")
         return HTTPStatus.ok
