6f2094a627f04adacbe32af2886da5aee92f12e8
Ahmed Khalaf <pxlshpr@gmail.com>
2023-02-08 14:59:08 +0500

    tag increment

--- backup.sh
+++ backup.sh
@@ -1,9 +1,16 @@
 unixtime=$(date +%s)
 cd /home/ahmed/PrepBackups
+
 #file="prep-backup-${unixtime}.sql"
-file="prep-backup.sql"
-pg_dump -U pxlshpr prep --exclude-table-data "preset_foods" > $file
-echo "         💾 Backup saved to: ${file}"
+#file="prep-backup.sql"
+#pg_dump -U pxlshpr prep --exclude-table-data "preset_foods" > $file
+
+fileData="prep-data.sql"
+fileSchema="prep-data.sql"
+pg_dump -U pxlshpr prep --schema-only > $fileSchema
+pg_dump -U pxlshpr prep --data-only --exclude-table-data "preset_foods" > $fileData
+
+echo "         💾 Backup saved to: ${fileData} and ${fileSchema}"
 git add .
 git commit -a -m "added backup"
 git push
